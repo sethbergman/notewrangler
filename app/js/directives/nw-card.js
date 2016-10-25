e5f63d605a7f99bf33a8f29523c5187900d0d135
Seth Bergman <seth.atxwebs@gmail.com>
2016-10-25 18:00:25 -0500

    section 2 complete

--- app/js/directives/nw-card.js
+++ app/js/directives/nw-card.js
@@ -2,10 +2,16 @@ angular.module('NoteWrangler')
 .directive('nwCard', function() {
   return {
     restrict: 'E',
-    templateUrl: 'templates/directives/nw-card.html',
+    templateUrl: './templates/directives/nw-card.html',
     scope: {
       header: '=',
-      description: '='
+      description: '=',
+      tweeted: '='
+    },
+    link: function(scope, element) {
+      if(scope.tweeted) {
+        element.addClass('tweeted');
+      }
     }
   };
 });
