a3ba180366752dca0d2c322cd88484e4db6fc047
Seth Bergman <seth.atxwebs@gmail.com>
2016-10-25 17:27:53 -0500

    Isolating

--- app/js/directives/nw-card.js
+++ app/js/directives/nw-card.js
@@ -1,10 +1,11 @@
-angular.module('NoteWrangler').directive('nwCard', [function() {
+angular.module('NoteWrangler')
+.directive('nwCard', function() {
   return {
     restrict: 'E',
     templateUrl: 'templates/directives/nw-card.html',
-    controller: function($scope) {
-      $scope.header = 'Note Title';
-      $scope.description = 'A lovely note description.';
-      }
+    scope: {
+      header: '=',
+      description: '='
+    }
   };
-}]);
+});
