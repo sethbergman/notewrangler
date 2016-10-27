f85f6dff5f97c85ca5d0f5f51553c576e2c86c5f
Seth Bergman <seth.atxwebs@gmail.com>
2016-10-27 04:17:49 -0500

    Section 4 complete!

---
+++ app/js/directives/title.js
@@ -0,0 +1,15 @@
+angular.module('NoteWrangler')
+.directive('popover', function($timeout) {
+  return function(scope, element, attrs) {
+    $timeout(function() {
+      $(element).popover({
+        trigger: 'hover',
+        content: attrs.popover
+      });
+    });
+
+    scope.$on('$destroy', function() {
+      $(element).popover('destroy');
+    });
+  };
+});
--- app/js/routes.js
+++ app/js/routes.js
@@ -1,15 +1,20 @@
-angular.module('NoteWrangler', ['ngRoute'])
+angular.module('NoteWrangler')
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider
     .when('/', {
-      redirectTo: '/users'
-    })
-    .when('/notes', {
-      templateUrl: 'templates/pages/notes/index.html',
-      controller: 'NotesIndexController'
+      redirectTo: '/notes'
     })
     .when('/users', {
-      templateUrl: 'templates/pages/users/index.html'
+      templateUrl: 'templates/pages/users/index.html',
+      controller: 'UsersIndexController'
+    })
+    .when('/users/:id', {
+      templateUrl: 'templates/pages/users/show.html',
+      controller: 'UsersShowController'
+    })
+    .when('/notes', {
+      templateUrl: '/templates/pages/notes/index.html',
+      controller: 'NotesIndexController'
     })
     .when('/notes/new', {
       templateUrl: 'templates/pages/notes/edit.html',
@@ -19,7 +24,15 @@ angular.module('NoteWrangler', ['ngRoute'])
       templateUrl: 'templates/pages/notes/show.html',
       controller: 'NotesShowController'
     })
+    .when('/notes/:id/edit', {
+      templateUrl: 'templates/pages/notes/edit.html',
+      controller: 'NotesEditController'
+    })
+    .when('/tweeted', {
+      templateUrl: 'templates/pages/notes/tweeted.html',
+      controller: 'TweetedController'
+    })
     .otherwise({
-      redirectTo: '/notes'
+      redirectTo: '/'
     });
 }]);
