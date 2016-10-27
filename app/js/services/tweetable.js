f39578a643a7a820f2ce610625934ad586e95b0a
Seth Bergman <seth.atxwebs@gmail.com>
2016-10-26 20:37:02 -0500

    save

--- app/js/services/tweetable.js
+++ app/js/services/tweetable.js
@@ -1,6 +1,7 @@
 angular.module('NoteWrangler')
 .factory('Tweetable', ['$http', function TweetableFactory($http) {
-    var characterLength = 140;
+  var characterLength = 140;
+
   return function(potentialTweet) {
     return $http({
       method: 'POST',
@@ -12,5 +13,28 @@ angular.module('NoteWrangler')
     });
   };
 }]);
+
+// angular.module('NoteWrangler')
+// .provider('Tweetable', function TweetableProvidr() {
+//   this.$get = function($http){
+//     return function(){
+//       return function(characterLength){
+//         return
+//       }
+//     }
+//   });
+//   var characterLength = 144;
+//
+//   return function(potentialTweet) {
+//     return $http({
+//       method: 'POST',
+//       url: 'http://gentle-spire-1153.herokuapp.com/tweet',
+//       data: {
+//         description: potentialTweet,
+//         maxLength: characterLength
+//       }
+//     });
+//   };
+// }]);
 
 // curl -H 'Content-Type: application/json' -d '{"description":"Example of description"}' http://localhost:3000/tweet
