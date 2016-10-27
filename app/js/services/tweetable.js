aa5490951fb2041cdafd673eba8f405d75e42bc1
Seth Bergman <seth.atxwebs@gmail.com>
2016-10-26 23:51:13 -0500

    refactored tweetable service from factory to provider

--- app/js/services/tweetable.js
+++ app/js/services/tweetable.js
@@ -1,8 +1,11 @@
 angular.module('NoteWrangler')
-.factory('Tweetable', ['$http', function TweetableFactory($http) {
-  var characterLength = 140;
-
-  return function(potentialTweet) {
+.provider('Tweetable', function TweetableProvider() {
+  var characterLength = 144;
+  this.setLength = function(maxLength){
+    characterLength = maxLength;
+  };
+  this.$get = function($http) {
+    return function(potentialTweet) {
     return $http({
       method: 'POST',
       url: 'http://gentle-spire-1153.herokuapp.com/tweet',
@@ -12,29 +15,7 @@ angular.module('NoteWrangler')
       }
     });
   };
-}]);
-
-// angular.module('NoteWrangler')
-// .provider('Tweetable', function TweetableProvidr() {
-//   this.$get = function($http){
-//     return function(){
-//       return function(characterLength){
-//         return
-//       }
-//     }
-//   });
-//   var characterLength = 144;
-//
-//   return function(potentialTweet) {
-//     return $http({
-//       method: 'POST',
-//       url: 'http://gentle-spire-1153.herokuapp.com/tweet',
-//       data: {
-//         description: potentialTweet,
-//         maxLength: characterLength
-//       }
-//     });
-//   };
-// }]);
+ };
+});
 
 // curl -H 'Content-Type: application/json' -d '{"description":"Example of description"}' http://localhost:3000/tweet
